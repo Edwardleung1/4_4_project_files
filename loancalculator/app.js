81375b9cd521b8fff5053f1b2779ac56782009bc
Edward Leung <edward_leung_12@hotmail.com>
2020-08-04 20:24:11 +0100

    added an error and clear error function

--- loancalculator/app.js
+++ loancalculator/app.js
@@ -4,7 +4,7 @@ document
   .addEventListener("submit", calculateResults);
 
 // Calculate Results
-function calculateResults() {
+function calculateResults(e) {
   console.log("Calculating...");
   // UI Form Variables
   const amount = document.getElementById("amount");
@@ -15,7 +15,7 @@ function calculateResults() {
   const totalPayment = document.getElementById("total-payment");
   const totalInterest = document.getElementById("total-interest");
   // Calculate Values
-  const principal = parseFloat(amount.values);
+  const principal = parseFloat(amount.value);
   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
   const calculatedPayments = parseFloat(years.value) * 12;
   // Calculate Monthly Payments
@@ -28,8 +28,35 @@ function calculateResults() {
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
   } else {
-    console.log("Please check your numbers");
+    showError("Please check your numbers");
   }
 
   e.preventDefault(); // Need to prevent the default form behaviour
+}
+
+// Show Error
+function showError(error) {
+  // Create a div
+  const errorDiv = document.createElement("div");
+
+  // Get elements
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
+
+  // Add class
+  errorDiv.className = "alert alert-danger";
+
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+// Clear error
+function clearError() {
+  document.querySelector(".alert").remove();
 }
