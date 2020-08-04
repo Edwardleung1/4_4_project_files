939b8f4155455865cb0d280480274a06ed848875
Edward Leung <edward_leung_12@hotmail.com>
2020-08-04 18:53:00 +0100

    added more variables to the UI

--- loancalculator/app.js
+++ loancalculator/app.js
@@ -10,6 +10,10 @@ function calculateResults() {
   const amount = document.getElementById("amount");
   const interest = document.getElementById("interest");
   const years = document.getElementById("years");
+  // Results Variables
+  const monthlyPayment = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
 
   e.preventDefault(); // Need to prevent the default form behaviour
 }
